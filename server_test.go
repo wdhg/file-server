010195fdb6ddee73fa401d210d19e5fce35fb568
wdhg <wdhgrant@gmail.com>
2018-06-17 16:05:20 +0100

    test error returning and file creation

--- server_test.go
+++ server_test.go
@@ -6,8 +6,17 @@ import (
 )
 
 func TestCreateFile(t *testing.T) {
-	Create("test.txt")
-	if _, err := os.Stat("files/test.txt"); os.IsNotExist(err) {
+	// test file creating
+	err := Create("test.txt")
+	if err != nil {
+		t.Error("Throwing error on making file for first time")
+	}
+	if _, err = os.Stat("files/test.txt"); os.IsNotExist(err) {
 		t.Error("File not created")
+	}
+	// test attempting to recreate files
+	err = Create("test.txt")
+	if err == nil {
+		t.Error("Recreating file not throwing error")
 	}
 }
