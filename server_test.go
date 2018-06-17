5009068f28c39ad7a9c85e20279928c1814cf2e8
wdhg <wdhgrant@gmail.com>
2018-06-17 16:18:26 +0100

    add content writing tests

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 )
@@ -11,15 +12,19 @@ func TestCreateFile(t *testing.T) {
 	os.Mkdir("files/", os.ModePerm)
 
 	// test file creating
-	err := Create("test.txt")
+	err := Create("test.txt", "test file\n")
 	if err != nil {
 		t.Error("Throwing error on making file for first time")
 	}
 	if _, err = os.Stat("files/test.txt"); os.IsNotExist(err) {
 		t.Error("File not created")
 	}
+	// check file contents
+	if dat, _ := ioutil.ReadFile("files/test.txt"); string(dat) != "test file\n" {
+		t.Error("File content not being writen")
+	}
 	// test attempting to recreate files
-	err = Create("test.txt")
+	err = Create("test.txt", "")
 	if err == nil {
 		t.Error("Recreating file not throwing error")
 	}
