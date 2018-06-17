8a3b563a66e1c2d09d4a3d24166516cd3d25ed3c
wdhg <wdhgrant@gmail.com>
2018-06-17 16:42:38 +0100

    loop over tests

--- server_test.go
+++ server_test.go
@@ -6,26 +6,36 @@ import (
 	"testing"
 )
 
+var testFiles = []struct {
+	name     string
+	contents string
+}{
+	{"test.txt", "test file\n"},
+}
+
 func TestCreateFile(t *testing.T) {
 	// setup code. clear files
 	os.RemoveAll("files/")
 	os.Mkdir("files/", os.ModePerm)
 
-	// test file creating
-	err := Create("test.txt", "test file\n")
-	if err != nil {
-		t.Error("Throwing error on making file for first time")
-	}
-	if _, err = os.Stat("files/test.txt"); os.IsNotExist(err) {
-		t.Error("File not created")
-	}
-	// test file contents
-	if dat, _ := ioutil.ReadFile("files/test.txt"); string(dat) != "test file\n" {
-		t.Error("File content not being writen")
-	}
-	// test attempting to recreate files
-	err = Create("test.txt", "")
-	if err == nil {
-		t.Error("Recreating file not throwing error")
+	for _, testFile := range testFiles {
+		file, contents := testFile.name, testFile.contents
+		// test file creating
+		err := Create(file, contents)
+		if err != nil {
+			t.Errorf("Throwing error on making %s for first time", file)
+		}
+		if _, err = os.Stat("files/" + file); os.IsNotExist(err) {
+			t.Errorf("%s not created", file)
+		}
+		// test file contents
+		if dat, _ := ioutil.ReadFile("files/" + file); string(dat) != contents {
+			t.Errorf("%s contents not being writen", file)
+		}
+		// test attempting to recreate files
+		err = Create(file, contents)
+		if err == nil {
+			t.Errorf("Recreating %s not throwing error", file)
+		}
 	}
 }
