dad16f6fe35d18d1838ca9e11134ba9405fe84a7
wdhg <wdhgrant@gmail.com>
2018-06-17 21:12:47 +0100

    add directory making

--- server.go
+++ server.go
@@ -4,16 +4,27 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const dir = "files/"
 
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
 // Create is called on POST requests to create new files
 func Create(filename string, contents string) error {
-	// append "files/" to start
 	filename = dir + filename
-	if _, err := os.Stat(filename); err == nil {
+	// check if file already exists
+	if pathExists(filename) {
 		return errors.New("File already exists")
+	}
+	dir := filepath.Dir(filename)
+	// create directory if it doesn't exist
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, os.ModePerm)
 	}
 	file, _ := os.Create(filename)
 	fmt.Fprintf(file, contents)
