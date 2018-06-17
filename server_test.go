a7df30b017713fd4bba69da4eb8bbd3575fa7d0c
wdhg <wdhgrant@gmail.com>
2018-06-17 21:58:23 +0100

    refactor test code and add invalid file testing

--- server_test.go
+++ server_test.go
@@ -9,9 +9,11 @@ import (
 var testFiles = []struct {
 	name     string
 	contents string
+	valid    bool
 }{
-	{"test.txt", "test file\n"},
-	{"test/test.txt", "test file\n"},
+	{"test.txt", "test file\n", true},
+	{"test/test.txt", "test file\n", true},
+	{"../test.txt", "test file\n", false},
 }
 
 func TestCreateFile(t *testing.T) {
@@ -21,22 +23,37 @@ func TestCreateFile(t *testing.T) {
 
 	for _, testFile := range testFiles {
 		file, contents := testFile.name, testFile.contents
-		// test file creating
-		err := Create(file, contents)
-		if err != nil {
-			t.Errorf("Throwing error on making %s for first time", file)
+		if testFile.valid {
+			doTestValidFile(t, file, contents)
+		} else {
+			doTestInvalidFile(t, file, contents)
 		}
-		if _, err = os.Stat("files/" + file); os.IsNotExist(err) {
-			t.Errorf("%s not created", file)
-		}
-		// test file contents
-		if dat, _ := ioutil.ReadFile("files/" + file); string(dat) != contents {
-			t.Errorf("%s contents not being writen", file)
-		}
-		// test attempting to recreate files
-		err = Create(file, contents)
-		if err == nil {
-			t.Errorf("Recreating %s not throwing error", file)
-		}
+	}
+}
+
+func doTestValidFile(t *testing.T, file, contents string) {
+	// test file creating
+	err := Create(file, contents)
+	if err != nil {
+		t.Errorf("Throwing error on making %s for first time", file)
+	}
+	if _, err = os.Stat("files/" + file); os.IsNotExist(err) {
+		t.Errorf("%s not created", file)
+	}
+	// test file contents
+	if dat, _ := ioutil.ReadFile("files/" + file); string(dat) != contents {
+		t.Errorf("%s contents not being writen", file)
+	}
+	// test attempting to recreate files
+	err = Create(file, contents)
+	if err == nil {
+		t.Errorf("Recreating %s not throwing error", file)
+	}
+}
+
+func doTestInvalidFile(t *testing.T, file, contents string) {
+	err := Create(file, contents)
+	if err == nil {
+		t.Errorf("Can create file %s", file)
 	}
 }
