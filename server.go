81f7da1c2b1d5b6e58c7852a211f802b3ff228d5
wdhg <wdhgrant@gmail.com>
2018-06-17 16:05:52 +0100

    add test passing code

--- server.go
+++ server.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
-func Create(file string) {
-	os.Create("files/" + file)
+const dir = "files/"
+
+// Create is called on POST requests to create new files
+func Create(file string) error {
+	// append "files/" to start
+	file = dir + file
+	if _, err := os.Stat(file); err == nil {
+		return errors.New("File already exists")
+	}
+	os.Create(file)
+	return nil
 }
 
 func main() {
