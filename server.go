8050b77d04350eb123f7c8dada511cfb6ddb99b8
wdhg <wdhgrant@gmail.com>
2018-06-17 16:18:35 +0100

    add content writing

--- server.go
+++ server.go
@@ -9,13 +9,14 @@ import (
 const dir = "files/"
 
 // Create is called on POST requests to create new files
-func Create(file string) error {
+func Create(filename string, contents string) error {
 	// append "files/" to start
-	file = dir + file
-	if _, err := os.Stat(file); err == nil {
+	filename = dir + filename
+	if _, err := os.Stat(filename); err == nil {
 		return errors.New("File already exists")
 	}
-	os.Create(file)
+	file, _ := os.Create(filename)
+	fmt.Fprintf(file, contents)
 	return nil
 }
 
