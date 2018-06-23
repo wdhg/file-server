5a120110a987edd1e2b63a6cf62d2a0e262cdcd0
wdhg <wdhgrant@gmail.com>
2018-06-23 11:15:30 +0100

    refactor and rewrite server tests

--- server_test.go
+++ server_test.go
@@ -13,106 +13,75 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func makeFile(file File) {
+func makeFile(file File, writeContents bool) {
 	os.Mkdir(dir+filepath.Dir(file.path), os.ModePerm)
 	fileWriter, _ := os.Create(dir + file.path)
-	fmt.Fprintf(fileWriter, file.contents)
+	if writeContents {
+		fmt.Fprintf(fileWriter, file.contents)
+	}
+}
+
+func testMethod(method string, files []File) func(*testing.T) {
+	return func(t *testing.T) {
+		os.Mkdir(dir, os.ModePerm)
+		defer os.RemoveAll(dir)
+		gin.SetMode(gin.TestMode)
+		router := CreateRouter()
+		writer := httptest.NewRecorder()
+
+		for _, file := range files {
+			if !file.valid {
+				continue
+			}
+
+			// if the server isn't going to make the file itself
+			if method != http.MethodPost {
+				makeFile(file, method != http.MethodPut)
+			}
+
+			// create the url for the request
+			requestURL := "/files/" + file.path
+			if method == http.MethodPost || method == http.MethodPut {
+				// add the url contents parameter
+				URL, _ := url.Parse(requestURL)
+				params := url.Values{}
+				params.Add("contents", file.contents)
+				URL.RawQuery = params.Encode()
+				requestURL = URL.String()
+			}
+
+			req, _ := http.NewRequest(method, requestURL, nil)
+			router.ServeHTTP(writer, req)
+			// test status code is correct
+			if writer.Code != http.StatusOK {
+				t.Errorf("Receiving error code on valid request")
+			}
+			// run test depending on method
+			switch method {
+			case http.MethodGet:
+				if writer.Body.String() != file.contents {
+					t.Errorf("Served file contents for %s are incorrect", file.path)
+				}
+			case http.MethodPost:
+				if dat, _ := ioutil.ReadFile(dir + file.path); string(dat) != file.contents {
+					t.Errorf("Created file %s doesn't contain correct contents", file.path)
+				}
+			case http.MethodPut:
+				if dat, _ := ioutil.ReadFile(dir + file.path); string(dat) != file.contents {
+					t.Errorf("Updated file %s doesn't contain correct contents", file.path)
+				}
+			case http.MethodDelete:
+				if _, err := os.Stat(dir + file.path); !os.IsNotExist(err) {
+					t.Errorf("%s not deleted", file.path)
+				}
+			}
+		}
+	}
 }
 
 func TestServer(t *testing.T) {
-	os.Mkdir(dir, os.ModePerm)
-	defer os.RemoveAll(dir)
-	gin.SetMode(gin.TestMode)
-	router := CreateRouter()
-	writer := httptest.NewRecorder()
-
-	// test get
-	for _, file := range getFiles {
-		if !file.valid {
-			continue
-		}
-
-		makeFile(file)
-		// test getting file contents through the server
-		req, _ := http.NewRequest(http.MethodGet, "/files/"+file.path, nil)
-		router.ServeHTTP(writer, req)
-		if writer.Code != http.StatusOK {
-			t.Errorf("Receiving error code on valid request")
-		}
-		if writer.Body.String() != file.contents {
-			t.Errorf("Served file contents for %s are incorrect", file.path)
-		}
-
-		os.RemoveAll(dir + filepath.Dir(file.path))
-	}
-
-	// test create
-	for _, file := range createFiles {
-		if !file.valid {
-			continue
-		}
-
-		// create url
-		URL, _ := url.Parse("/files/" + file.path)
-		params := url.Values{}
-		params.Add("contents", file.contents)
-		URL.RawQuery = params.Encode()
-		// test if server is creating the file correctly
-		req, _ := http.NewRequest(http.MethodPost, URL.String(), nil)
-		router.ServeHTTP(writer, req)
-		if writer.Code != http.StatusOK {
-			t.Errorf("Receiving error code on valid request")
-		}
-		if dat, _ := ioutil.ReadFile(dir + file.path); string(dat) != file.contents {
-			t.Errorf("Created file %s doesn't contain correct contents", file.path)
-		}
-
-		os.RemoveAll(dir + filepath.Dir(file.path))
-	}
-
-	// test update
-	for _, file := range updateFiles {
-		if !file.valid {
-			continue
-		}
-
-		// create url for encoding url parameters
-		URL, _ := url.Parse("/files/" + file.path)
-		params := url.Values{}
-		params.Add("contents", file.contents)
-		URL.RawQuery = params.Encode()
-
-		makeFile(file)
-		// test if server is update files
-		req, _ := http.NewRequest(http.MethodPut, URL.String(), nil)
-		router.ServeHTTP(writer, req)
-		if writer.Code != http.StatusOK {
-			t.Errorf("Receiving error code on valid request")
-		}
-		if dat, _ := ioutil.ReadFile(dir + file.path); string(dat) != file.contents {
-			t.Errorf("Updated file %s doesn't contain correct contents", file.path)
-		}
-
-		os.RemoveAll(dir + filepath.Dir(file.path))
-	}
-
-	// test delete
-	for _, file := range deleteFiles {
-		if !file.valid {
-			continue
-		}
-
-		makeFile(file)
-		// test if server deletes the file
-		req, _ := http.NewRequest(http.MethodDelete, "/files/"+file.path, nil)
-		router.ServeHTTP(writer, req)
-		if writer.Code != http.StatusOK {
-			t.Errorf("Receiving error code on valid request")
-		}
-		if _, err := os.Stat(dir + file.path); !os.IsNotExist(err) {
-			t.Errorf("%s not deleted", file.path)
-		}
-
-		os.RemoveAll(dir + filepath.Dir(file.path))
-	}
+	t.Run(http.MethodGet, testMethod(http.MethodGet, getFiles))
+	t.Run(http.MethodPost, testMethod(http.MethodPost, createFiles))
+	t.Run(http.MethodPut, testMethod(http.MethodPut, updateFiles))
+	t.Run(http.MethodDelete, testMethod(http.MethodDelete, deleteFiles))
 }
